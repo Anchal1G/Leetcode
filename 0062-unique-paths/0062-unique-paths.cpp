6dc06f70cbc5803c543698b4a1d1b8aa94a639a5
Anchal Gupta <88591615+Anchal1G@users.noreply.github.com>
2023-09-03 12:05:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.25%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int total = m+n-2;
+        int r = m-1;
+
+        double ans =1;
+        for(int i=1;i<=r;i++){
+            ans = ans*(total-r+i)/i;
+            
+        }
+        return (int)ans;
+    }
+};
